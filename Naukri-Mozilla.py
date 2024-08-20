5f8289b3cf5bd6d5ec0799d55847df7ab43489bf
faisalmalik47 <malikf951@gmail.com>
2024-08-20 16:17:53 +0530

    modified:   Naukri-Mozilla.py 	new file:   naukri_applied_jobs.csv 	modified:   naukriapplied.csv 	new file:   output.txt

--- Naukri-Mozilla.py
+++ Naukri-Mozilla.py
@@ -1,81 +1,93 @@
 import pandas as pd
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
 
-firstname=''                        #Add your LastName
-lastname=''                         #Add your FirstName
-joblink=[]                          #Initialized list to store links
-maxcount=50                         #Max daily apply quota for Naukri
-keywords=['','']                    #Add you list of role you want to apply
-location = ''                       #Add your location/city name for within India or remote
-applied =0                          #Count of jobs applied sucessfully
-failed = 0                          #Count of Jobs failed
-applied_list={
-    'passed':[],
-    'failed':[]
-}                                   #Saved list of applied and failed job links for manual review
+firstname = 'Faisal'
+lastname = 'Malik'
+joblink = []
+maxcount = 50
+keywords = ['aws devops', 'devops', 'cloud', 'aws']
+location = ['india', 'remote', 'delhi', 'ncr']
+applied = 0
+failed = 0
+applied_list = []
 
+profile_path = "/Users/faisalmalik/Library/Application Support/Firefox/Profiles/cnjnyp58.naukri"
+
+options = Options()
+options.profile = profile_path
 
 try:
-    profile = webdriver.FirefoxProfile("") #Add your Root directory path
-    driver = webdriver.Firefox(profile)
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 except Exception as e:
-    print('Webdriver exception')
+    print('Webdriver exception:', e)
+    exit()
+
 time.sleep(10)
 for k in keywords:
-    for i in range(2):
-        if location=='':
-            url = "https://www.naukri.com/"+k.lower().replace(' ','-')+"-"+str(i+1)
-        else: url = "https://www.naukri.com/"+k.lower().replace(' ','-')+"-jobs-in-"+location.lower().replace(' ','-')+"-"+str(i+1)
-        driver.get(url)
-        print(url)
-        time.sleep(3)
-        soup = BeautifulSoup(driver.page_source,'html5lib')
-        results = soup.find(class_='list')
-        job_elems = results.find_all('article',class_='jobTuple bgWhite br4 mb-8')
-        for job_elem in job_elems:
-            joblink.append(job_elem.find('a',class_='title fw500 ellipsis').get('href'))
-
-
-for i in joblink:
-    time.sleep(3)
-    driver.get(i)   
-    if applied <=maxcount:
-        try:
-            time.sleep(3)
-            driver.find_element_by_xpath("//*[text()='Apply']").click()
-            time.sleep(2)
-            applied +=1
-            applied_list['passed'].append(i)
-            print('Applied for ',i, " Count", applied)
-
-        except Exception as e: 
-            failed+=1
-            applied_list['failed'].append(i)
-            print(e, "Failed " ,failed)
-        try:    
-            if driver.find_element_by_xpath("//*[text()='Your daily quota has been expired.']"):
-                print('MAX Limit reached closing browser')
-                driver.close()
-                break
-            if driver.find_element_by_xpath("//*[text()=' 1. First Name']"):
-                driver.find_element_by_xpath("//input[@id='CUSTOM-FIRSTNAME']").send_keys(firstname)
-            if driver.find_element_by_xpath("//*[text()=' 2. Last Name']"):
-                driver.find_element_by_xpath("//input[@id='CUSTOM-LASTNAME']").send_keys(lastname);
-            if driver.find_element_by_xpath("//*[text()='Submit and Apply']"):
-                driver.find_element_by_xpath("//*[text()='Submit and Apply']").click()
-        except:
-            pass
+    for loc in location:
+        for i in range(1, 3):  # Loop through 2 pages per keyword and location
+            url = f"https://www.naukri.com/{k.lower().replace(' ', '-')}-jobs-in-{loc.lower().replace(' ', '-')}-{i}"
             
-    else:
-        driver.close()
+            driver.get(url)
+            print(f"Scraping URL: {url}")
+            time.sleep(3)
+
+            page_source = driver.page_source
+            soup = BeautifulSoup(page_source, 'html.parser')
+
+            # Find job link containers
+            job_elems = soup.find_all('div', class_='sjw__tuple')
+            
+            for job_elem in job_elems:
+                link_elem = job_elem.find('a', class_='title')
+                if link_elem:
+                    joblink.append(link_elem.get('href'))
+
+# Apply for jobs
+for job_url in joblink:
+    if applied >= maxcount:
+        print('Max job apply limit reached.')
         break
-print('Completed applying closing browser saving in applied jobs csv')
-try:
-    driver.close()
-except:pass
-csv_file = "naukriapplied.csv"
-final_dict= dict ([(k, pd.Series(v)) for k,v in applied_list.items()])
-df = pd.DataFrame.from_dict(final_dict)
-df.to_csv(csv_file, index = False)
+    
+    driver.get(job_url)
+    time.sleep(3)
+    
+    try:
+        # Wait for the apply button to be clickable
+        wait = WebDriverWait(driver, 10)
+        apply_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="apply-button"]')))
+        apply_button.click()
+        time.sleep(2)
+        
+        if driver.current_url != job_url:
+            applied += 1
+            applied_list.append({'status': 'passed', 'url': job_url})
+            print(f"Applied for job: {job_url}")
+        else:
+            failed += 1
+            applied_list.append({'status': 'failed', 'url': job_url})
+            print(f"Failed to apply for job: {job_url}")
+    
+    except Exception as e:
+        failed += 1
+        applied_list.append({'status': 'failed', 'url': job_url})
+        print(f"Exception occurred while applying for job: {job_url}")
+        print(e)
+
+# Close the browser
+print('Completed applying, closing browser, saving in applied jobs csv')
+driver.quit()
+
+# Save applied jobs to CSV
+csv_file = "naukri_applied_jobs.csv"
+df = pd.DataFrame(applied_list)
+df.to_csv(csv_file, index=False)
+print(f"Saved applied jobs to {csv_file}")
